e964b20eab0ae45da82e487b1e98e17ad5ac4985
Aya <aya.khellou@gmail.com>
2025-08-03 08:47:10 +0100

    add win function + restart function + game over + stying

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 let cells = document.querySelectorAll('.cell');
+let restart = document.querySelector('button.restart');
+let gameOver = false;
+
+// restart function
+restart.addEventListener('click', ()=>{
+
+    window.location.reload();
+    
+})
+//set wining cases:
+
+let winningCases = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 
 // check win function:
 
-function checkWin() {
+function checkWin(currentCell) {
     
-    // if:
-// => 1 2 3 / 4 5 6 / 7 8 9  => win
-// => 1 4 7 / 2 5 8 / 3 6 9 => win
-// =>  3 5 7 / 1 5 9 => win
+    winningCases.forEach(winCase => {
 
+        if (winCase.every(el => cells[el].textContent === currentCell.textContent)) {
+            console.log(`${currentCell.textContent} win`);
+            winCase.forEach(el => {
+                cells[el].style.backgroundColor = 'white';
+            });
+            gameOver = true;
+            setGameOver();
+        }
+
+    });
 }
-
-let win1 = [1,2,3];
 
 
 // Add click event listener to each cell
@@ -19,22 +46,30 @@ let win1 = [1,2,3];
 
 let turn = 'x';
 
-cells.forEach(cell => {
+cells.forEach((cell,i) => {
     cell.addEventListener('click', () => {
-        if (cell.textContent === "" && turn === 'x') {
+        if (cell.textContent === "" && turn === 'x' && !gameOver) {
             cell.textContent = 'X';
-            
-            console.log(cell.id);
+
+            checkWin(cell);
 
             turn = 'o';
             console.log(`it's your turn O`);
 
-        }else if(cell.textContent === "" && turn === 'o') {
+        }else if(cell.textContent === "" && turn === 'o' && !gameOver) {
             cell.textContent = 'O';
+
+            checkWin(cell);
+
             turn = 'x';
             console.log(`it's your turn X`);
         }
     });
 });
 
-
+function setGameOver(){
+    if (gameOver) {
+        console.log('game is over!!!!!!!!!!!');
+        
+    }
+}
